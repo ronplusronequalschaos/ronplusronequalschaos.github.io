177b3adabfb85e39ac3c937ef88553834140a93e
ronplusronequalschaos <ronplusronequalschaos@gmail.com>
2026-02-01 10:19:37 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const password = "im so silly";
 let input = window.prompt("Password please");
-input == password ? window.location.href = "labubububub.html" : window.location.href = "index3.html";
+input == password ? window.location.href = "index2.html" : window.location.href = "index3.html";
+
